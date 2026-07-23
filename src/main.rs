695609dc990f4b254f95cab946562fe7d616a5a9
yag1Rice <yag1@rice.edu>
2026-07-23 17:54:43 -0400

    parsing flags complete - Youssef

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
 mod shell;
+use shell::Shell;
+
+use clap::{CommandFactory, Parser};
+
+#[derive(Parser)]
+#[command(name = "shell", about = "Tiny Shell", override_usage = "shell [-hvp]")]
+struct Args {
+    #[arg(short, help = "do not emit a command prompt")]
+    prompt_flag: bool, // Flag to indicate to not display the prompt
+
+    #[arg(short, help = "print additional diagnostic information")]
+    verbose_flag: bool, // Flag to indicate whether to display verbose error messages
+}
 
 fn main() {
-    println!("Hello, world!");
+    if std::env::args().len() == 1 {
+        Args::command().print_help().unwrap();
+        return;
+    }
+
+    let args = Args::parse(); // Parse command-line arguments
+
+    let mut shell = Shell::new(args.prompt_flag, args.verbose_flag); // Create a new Shell instance
 }
--- src/shell.rs
+++ src/shell.rs
@@ -1,9 +1,9 @@
 use nix::unistd::Pid;
 
 // You may assume that these constants are large enough.
-const MAXLINE: usize = 1024;	  // max line size
-const MAXARGS: usize = 128;	  // max args on a command line
-const MAXJOBS: usize = 16;	  // max jobs at any point in time
+const MAXLINE: usize = 1024; // max line size
+const MAXARGS: usize = 128; // max args on a command line
+const MAXJOBS: usize = 16; // max jobs at any point in time
 const MAXJID: usize = 1 << 16; // max job ID
 
 // The job states are:
@@ -17,20 +17,30 @@ enum JobState {
 
 #[derive(Debug)]
 struct Job {
-    pid : Option<Pid>,	    // job PID
-	jid: usize,             // job ID [1, 2, ...]
-	state: JobState,        // UNDEF, FG, BG, or ST
-	cmdline: String,        // command line
+    pid: Option<Pid>, // job PID
+    jid: usize,       // job ID [1, 2, ...]
+    state: JobState,  // UNDEF, FG, BG, or ST
+    cmdline: String,  // command line
 }
 
-struct Shell {
+pub struct Shell {
     jobs: Vec<Job>,
     verbose_flag: bool,
     prompt_flag: bool,
+}
+
+impl Shell {
+    // TODO!: Placeholder for the Shell constructor.
+    pub fn new(prompt_flag: bool, verbose_flag: bool) -> Self {
+        Shell {
+            jobs: Vec::new(),
+            verbose_flag,
+            prompt_flag,
+        }
+    }
 }
 
 fn unix_error(msg: &str) {
     println!("{}: {}", msg, nix::errno::Errno::last());
     std::process::exit(1);
 }
-
